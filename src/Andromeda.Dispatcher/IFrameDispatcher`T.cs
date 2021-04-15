2ffca4d3151df34f26ed35f98ae2fdbcc6b9747d
thenameless314159 <thenameless314159@gmail.com>
2021-04-15 14:12:31 +0200

    updated auto implemented interface member

--- src/Andromeda.Dispatcher/IFrameDispatcher`T.cs
+++ src/Andromeda.Dispatcher/IFrameDispatcher`T.cs
@@ -5,7 +5,7 @@ namespace Andromeda.Dispatcher
 {
     public interface IFrameDispatcher<TMeta> : IFrameDispatcher where TMeta : class, IFrameMetadata
     {
-        new Task<DispatchResult> OnFrameReceivedAsync(in Frame frame, SenderContext context) { var typed = frame.AsTyped<TMeta>(); return OnFrameReceivedAsync(in typed, context); }
+        new Task<DispatchResult> OnFrameReceivedAsync(in Frame frame, SenderContext context) => OnFrameReceivedAsync(frame.AsTyped<TMeta>(), context);
         Task<DispatchResult> OnFrameReceivedAsync(in Frame<TMeta> frame, SenderContext context);
     }
 }
