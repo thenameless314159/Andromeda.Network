d44573cd5b064a9a35551cf0d9bdf9f76620d9c3
Brian Ding <Brian_Ding@outlook.com>
2021-10-18 21:17:02 +0800

    IFrameDecoder inherit IAsyncEnumerable<Frame>

--- src/Andromeda.Framing/PipeFrameDecoder.cs
+++ src/Andromeda.Framing/PipeFrameDecoder.cs
@@ -14,7 +14,7 @@ namespace Andromeda.Framing
     {
         public PipeFrameDecoder(Stream stream, IMetadataDecoder decoder) => (_decoder, _pipe) = (decoder, PipeReader.Create(stream));
         public PipeFrameDecoder(PipeReader pipe, IMetadataDecoder decoder) => (_decoder, _pipe) = (decoder, pipe);
-        
+
         /// <inheritdoc />
         public long FramesRead => Interlocked.Read(ref _framesRead);
 
@@ -35,11 +35,13 @@ namespace Andromeda.Framing
         /// <inheritdoc />
         public async IAsyncEnumerable<Frame> ReadFramesAsync([EnumeratorCancellation] CancellationToken token = default)
         {
-            if(_isConsuming) ThrowIfAlreadyConsuming();
+            if (_isConsuming) ThrowIfAlreadyConsuming();
             _isConsuming = true;
 
-            try {
-                while (!token.IsCancellationRequested) {
+            try
+            {
+                while (!token.IsCancellationRequested)
+                {
                     try { await ReadFrameAsync(false, token).ConfigureAwait(false); }
                     catch (ObjectDisposedException) { yield break; }
                     yield return _frame;
@@ -47,7 +49,7 @@ namespace Andromeda.Framing
             }
             finally { _isConsuming = false; }
         }
-        
+
         private async ValueTask<Frame> ReadFrameAsync(bool throwOnConsuming, CancellationToken token = default)
         {
             if (throwOnConsuming && _isConsuming) ThrowIfAlreadyConsuming();
@@ -55,7 +57,8 @@ namespace Andromeda.Framing
             if (!TryAdvanceToNextFrame()) throw new ObjectDisposedException(GetType().Name);
             var reader = _pipe ?? throw new ObjectDisposedException(GetType().Name);
 
-            try {
+            try
+            {
                 for (var attempt = 1; /* true */; attempt++)
                 {
                     var readResult = await reader.ReadAsync(token).ConfigureAwait(false);
@@ -63,7 +66,8 @@ namespace Andromeda.Framing
                     Trace.TraceWarning($"Couldn't read frame with a single attempt, current try: {attempt}.");
                 }
             }
-            catch (InvalidOperationException e) when (e.Message == "Reading is not allowed after reader was completed.") {
+            catch (InvalidOperationException e) when (e.Message == "Reading is not allowed after reader was completed.")
+            {
                 throw new ObjectDisposedException(GetType().Name, e);
             }
         }
@@ -76,9 +80,10 @@ namespace Andromeda.Framing
             if (_isCanceled) return false;
             var buffer = readResult.Buffer;
             _nextFrame = buffer.Start;
-            _hasAdvanced = false; 
+            _hasAdvanced = false;
 
-            if (buffer.TryParseFrame(_decoder, out frame)) { 
+            if (buffer.TryParseFrame(_decoder, out frame))
+            {
                 _nextFrame = frame.Payload.End; _framesRead++;
                 // If the payload is empty there's no need for the reader to hold on to the bytes.
                 return !frame.IsPayloadEmpty() || TryAdvanceToNextFrame();
@@ -96,7 +101,7 @@ namespace Andromeda.Framing
         {
             if (_hasAdvanced) return true;
             if (_pipe is null) return false;
-            if (!_pipe.TryAdvanceTo(_nextFrame)) 
+            if (!_pipe.TryAdvanceTo(_nextFrame))
                 return false;
 
             _hasAdvanced = true;
@@ -108,7 +113,8 @@ namespace Andromeda.Framing
 
         public virtual ValueTask DisposeAsync() { Dispose(); return default; }
 
-        public virtual void Dispose() {
+        public virtual void Dispose()
+        {
             var pipe = Interlocked.Exchange(ref _pipe, null);
             if (pipe is null) return;
             _isThisCompleted = true;
@@ -117,6 +123,11 @@ namespace Andromeda.Framing
             // that own the pipe, but this is not within the scope of this library so maybe we should...
             pipe.CancelPendingRead();
             GC.SuppressFinalize(this);
+        }
+
+        public IAsyncEnumerator<Frame> GetAsyncEnumerator(CancellationToken cancellationToken = default)
+        {
+            return ReadFramesAsync(cancellationToken).GetAsyncEnumerator(cancellationToken);
         }
     }
 }
